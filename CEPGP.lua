27b3e35bb2b3b287dc6821b4ea9b84dbef66aaf9
TheOneReed <ariacilon@gmail.com>
2016-12-30 21:15:48 +0900

    Sanitized Alumian's Inputs for him

--- CEPGP.lua
+++ CEPGP.lua
@@ -344,6 +344,7 @@ function CEPGP_ListButton_OnClick()
 			CEPGP_context_popup_title:SetText("Add EP/GP to " .. name);
 			CEPGP_context_popup_desc:SetText("Adding EP");
 			CEPGP_context_amount:SetText("0");
+			CEPGP_context_amount:SetNumeric(enable);
 			CEPGP_context_popup_confirm:SetScript('OnClick', function()
 																PlaySound("gsTitleOptionExit");
 																HideUIPanel(CEPGP_context_popup);
@@ -367,6 +368,7 @@ function CEPGP_ListButton_OnClick()
 			CEPGP_context_popup_title:SetText("Add Guild EP");
 			CEPGP_context_popup_desc:SetText("Adds EP to all guild members");
 			CEPGP_context_amount:SetText("0");
+			CEPGP_context_amount:SetNumeric(enable);
 			CEPGP_context_popup_confirm:SetScript('OnClick', function()
 																PlaySound("gsTitleOptionExit");
 																HideUIPanel(CEPGP_context_popup);
@@ -386,6 +388,7 @@ function CEPGP_ListButton_OnClick()
 			CEPGP_context_popup_title:SetText("Decay Guild EPGP");
 			CEPGP_context_popup_desc:SetText("Decays EPGP standings by a percentage\nValid Range: 0-100");
 			CEPGP_context_amount:SetText("0");
+			CEPGP_context_amount:SetNumeric(enable);
 			CEPGP_context_popup_confirm:SetScript('OnClick', function()
 																PlaySound("gsTitleOptionExit");
 																HideUIPanel(CEPGP_context_popup);
@@ -428,6 +431,8 @@ function CEPGP_ListButton_OnClick()
 			CEPGP_context_popup_header:SetText("Raid Moderation");
 			CEPGP_context_popup_title:SetText("Add EP/GP to " .. name);
 			CEPGP_context_popup_desc:SetText("Adding EP");
+			CEPGP_context_amount:SetText("0");
+			CEPGP_context_amount:SetNumeric(enable);
 			CEPGP_context_popup_confirm:SetScript('OnClick', function()
 																PlaySound("gsTitleOptionExit");
 																HideUIPanel(CEPGP_context_popup);
@@ -450,6 +455,8 @@ function CEPGP_ListButton_OnClick()
 			CEPGP_context_popup_header:SetText("Raid Moderation");
 			CEPGP_context_popup_title:SetText("Award Raid EP");
 			CEPGP_context_popup_desc:SetText("Adds an amount of EP to the entire raid");
+			CEPGP_context_amount:SetText("0");
+			CEPGP_context_amount:SetNumeric(enable);
 			CEPGP_context_popup_confirm:SetScript('OnClick', function()
 																PlaySound("gsTitleOptionExit");
 																HideUIPanel(CEPGP_context_popup);
